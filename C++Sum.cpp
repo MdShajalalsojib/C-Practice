c66fb920e7107cab11f7732a9b7d49639fad21d7
Md. Shajalal Sojib <shahjalalsojib088@gmail.com>
2024-11-17 17:10:28 +0600

    Create C++Sum.cpp
    Just two Number is sum

---
+++ C++Sum.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int firstNumber = 88;
+  int secondNumber = 38;
+  int sum = 0;
+  sum = firstNumber+secondNumber;
+  cout << sum ;
+   return 0;
+}
