c13ce38c0b51fb1db508a1ee6b84a57547a67da7
Md. Shajalal Sojib <shahjalalsojib088@gmail.com>
2024-11-17 16:06:21 +0600

    Create std_library.cpp
    std library

---
+++ std_library.cpp
@@ -0,0 +1,9 @@
+// std is this library if I use std before cout then after #include<iostream>
+using namespace std no need to use
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!";
+  return 0;
+}
