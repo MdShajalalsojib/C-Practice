d99455a1b9fb496eabd3e686de6d1b7cc91c233d
Md. Shajalal Sojib <shahjalalsojib088@gmail.com>
2024-11-17 16:57:25 +0600

    Create C++Variable.cpp

---
+++ C++Variable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int myId = 88;
+    char  mynamefirst='S';
+    double myNumber = 88.80;
+    bool myBoolean = true;
+    string myName = "Md.Shajalal";
+    cout <<myId <<"\n";
+    cout <<mynamefirst <<"\n";
+    cout <<myNumber <<"\n";
+    cout <<myBoolean <<"\n";
+    cout<<myName <<"\n";
+}
+    
